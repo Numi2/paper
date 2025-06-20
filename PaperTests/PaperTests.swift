d5d66de16bf9e4586fd50ece420f4f7028bf28b1
T <numit22@gmail.com>
2025-06-20 17:05:12 +0200

    1

---
+++ PaperTests/PaperTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaperTests.swift
+//  PaperTests
+//
+//  Created by T on 6/20/25.
+//
+
+import Testing
+@testable import Paper
+
+struct PaperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
